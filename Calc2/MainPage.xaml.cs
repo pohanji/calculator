4e4a9e1ded70ab051e7f48c2b3b05d87f5cac50f
Jiří Pohančeník <jiri.pohancenik@1pf.cz>
2020-10-11 21:42:34 +0200

    Finished application

---
+++ Calc2/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Calc2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnButtonClick(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+            int number;
+            var text = button.Text;
+
+            var isNumber = Int32.TryParse(text, out number);
+            if (isNumber)
+            {
+                if (area.Text == "0")
+                    area.Text = text;
+                else
+                {
+                    area.Text += text;
+                }
+            }
+            else
+            {
+                if (text == "=")
+                    area.Text = new DataTable().Compute(area.Text, null).ToString();
+                else
+                    area.Text += text;
+            }
+        }
+    }
+}
